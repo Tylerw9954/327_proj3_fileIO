53a9019651994f04ddcf0d101b5006936994568c
Tyler Warburton <tylerw9954@gmail.com>
2019-11-02 17:45:09 -0400

    getting 95

--- FileIO.cpp
+++ FileIO.cpp
@@ -1,3 +1,4 @@
+
 /*
  * FileReader.cpp
  *
@@ -11,14 +12,59 @@
 
 using namespace std;
 
+//namespace KP_FileIO{
+//trys to open filename, and return its contents in contents
+//it will strip out only \n's (char return\line feed)
+//returns:
+//SUCCESS all worked, contents string contains all in the file
+//COULD_NOT_OPEN_FILE_TO_READ contents string is cleared
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
-	//TODO fill in
+	fstream inS;
+	contents = "";
+	inS.open(filename.c_str());
+	if(!inS.is_open())
+		return COULD_NOT_OPEN_FILE_TO_READ;
+
+	string curline = "";
+	while(!inS.eof()){
+		//pretty sure this follows requirements
+		getline(inS, curline);
+		contents += curline;
+	}
+	//close and exit successfully
+	inS.close();
+	return SUCCESS;
 }
 
+
+//trys to open filename, and serialize  myEntryVector contents to it
+//returns:
+//SUCCESS all worked, filename contains myEntryVector strings
+//COULD_NOT_OPEN_FILE_TO_WRITE
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	fstream outS;
+	outS.open(filename.c_str());
+
+	//Shouldn't happen but ok?
+	if(!outS.is_open())
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+
+	//adds all non "" to cout with each entry being its own line
+	for(string add: myEntryVector){
+		if(add != ""){
+			outS << add << endl;
+		}
+	}
+	//close and exit successfully
+	outS.close();
+	return SUCCESS;
 }
+//}
+
+
+
+
 
 
